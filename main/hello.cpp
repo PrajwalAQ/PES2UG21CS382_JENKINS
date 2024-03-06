bab49e3c467d19fb23aac151e28ea66a9df8426e
PrajwalAQ <112750126+PrajwalAQ@users.noreply.github.com>
2024-03-07 00:05:40 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Prajwal Quadras\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "Changes made\n";
   return 0;
 }
